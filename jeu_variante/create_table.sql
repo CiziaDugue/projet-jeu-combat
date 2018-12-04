3e5bcc4cf6d557ab11007258264bc57947789365
Cizia Dugué <cizia.dugue@etu.univ-st-etienne.fr>
2018-12-04 11:12:20 +0100

    ajout du fichier sql create_table

---
+++ jeu_variante/create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `personnages` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `degats` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `experience` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `niveau` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `nbCoups` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `dateDernierCoup` date NOT NULL DEFAULT '1900-01-01',
+  `dateDerniereConnexion` date NOT NULL DEFAULT '1900-01-01',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nom` (`nom`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
