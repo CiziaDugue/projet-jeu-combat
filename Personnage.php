760f5a81008c988e6828794f5dc78a00ae5986cb
Cizia Dugué <cizia.dugue@telecom-st-etienne.fr>
2019-01-14 13:35:00 +0100

    orga fichiers

--- Personnage.php
+++ Personnage.php
@@ -1,154 +1,183 @@
 <?php
-/*Classe pour instancier des objets personnages*/
+/**************CLASSE POUR INSTANCIER DES OBJETS PERSONNAGES**************/
 // Elle définit les caractéristiques et les fonctionnalités des objets instanciés
-
-class Personnage {
-	
-	/*Définition des attributs de l'objet*/
-	// Ce sont les caractériques du personnage à instancier
-	// private = accessible seulement par le code écrit dans la classe = encapsulation
-	
-	private $_id;
-	private $_nom;
-	private $_forcePerso;
-	private $_degats;
-	private $_niveau;
-	private $_experience;
-	
-	
-	/*Définition de constantes de classe*/
-	
-	
-	/*Constructeur = méthode pour appliquer des règles à la contruction d'objet*/
-	// On veut créer des objets en donnant des valeurs aux attributs déclarés précédemment
-	// public = accessible partout, pour pouvoir appeler les méthodes sur l'objet à l'extérieur de la classe / de l'objet ?
-	// this désigne l'objet courrant
-	
-	public function __construct(array $donnees) {
-		
+class Personnage
+{
+    private $_id;
+    private $_nom;
+    private $_degats;
+    private $_experience;
+    private $_niveau;
+    private $_nbCoups;
+    private $_dateDernierCoup;
+    private $_dateDerniereConnexion;
+     
+    const CEST_MOI = 1;
+    const PERSONNAGE_TUE = 2;
+    const PERSONNAGE_FRAPPE = 3;
+    const PAS_AUJOURDHUI = 4;
+     
+    public function __construct(array $donnees)
+    {
         $this->hydrate($donnees);
-	}
-	
-    
-    /*Méthode pour hydrater les objets = donner des valeurs à leurs propriétés*/
-    // Un tableau de données doit être passé à la fonction (d'où le mot-clé array)
-	// On veut attribuer des valeurs de la bdd aux propriétés de l'objet avec les setters car ils controlent l'intégrité des valeurs
-
-	public function hydrate(array $donnees) {
-		
-		// Boucle pour parcourir le tableau $donnees
-		foreach ($donnees as $key => $value) {
-			
-			// Déclaration d'une variable $method avec la même syntaxe que nos setters
-			// ucfirst pour passer la 1ere lettre de la clé en MAJ
-			$method = 'set'.ucfirst($key);
-
-			// Vérification : si la méthode existe
-			if (method_exists($this, $method)) {
-				
-				// ...on l'appelle avec la valeur du tableau en argument
-				$this->$method($value);
-			}
-		}
-	}
-
-	/*Définition des getters*/
-	// Méthodes qui retournent la valeur des propriétés de l'objet (this désigne l'objet courrant)
-	
-	public function id() {
-		return $this->_id;
-	}
-	public function nom() {
-		return $this->_nom;
-	}
-	public function forcePerso() {
-		return $this->_forcePerso;
-	}
-	public function degats() {
-		return $this->_;
-	}
-	public function niveau() {
-		return $this->_niveau;
-	}
-	public function experience() {
-		return $this->_experience;
-	}
-	
-	/*Définition des setters*/
-	// Méthodes pour modifier la valeur des propriétés de l'objet (this désigne l'objet courrant)
-	// Vérification de la validité des données
-	
-	public function setId($id) {
-		
-		//On convertit l'argument en nombre entier (ne fait rien si entier, sinon donne 0)
-		
-		$id = (int) $id;
-		
-		//On vérifie s'il est strictement positif
-		
-		if ($id > 0) {
-			
-			//Si c'est le cas, on assigne la valeur à l'attribut correspondant
-			
-			$this->_id = $id;
-		}
-	}
-	
-	public function setNom($nom) {
-		
-		//On vérifie qu'il s'agit bien d'une chaîne de caractères
-		
-		if (is_string($nom)) {
-			
-			$this->_nom = $nom;
-		}
-	}
-	
-	public function setForcePerso($forcePerso) {
-		
-		$forcePerso = (int) $forcePerso;
-		
-		//On vérifie s'il est strictement positif
-		
-		if ($forcePerso >= 1 && $forcePerso <= 100) {
-			
-			$this->_forcePerso = $forcePerso;
-		}
-	}
-	
-	public function setDegats($degats) {
-		
-		$degats = (int) $degats;
-		
-		//On vérifie s'il est strictement positif
-		
-		if ($degats >= 1 && $degats <= 100) {
-			
-			$this->_degats = $degats;
-		}
-	}
-	
-	public function setNiveau($niveau) {
-		
-		$niveau = (int) $niveau;
-		
-		//On vérifie s'il est strictement positif
-		
-		if ($niveau >= 1 && $niveau <= 100) {
-			
-			$this->_niveau = $niveau;
-		}
-	}
-	
-	public function setExperience($experience) {
-		
-		$experience = (int) $experience;
-		
-		//On vérifie s'il est strictement positif
-		
-		if ($experience >= 1 && $experience <= 100) {
-			
-			$this->experience = $experience;
-		}
-	}
+    }
+     
+ 
+    public function frapper(Personnage $perso)
+    {
+        if ($this->id() == $perso->id()){
+            return self::CEST_MOI;
+        }
+         
+        $now = new DateTime('NOW');
+        $diff = $this->dateDernierCoup()->diff($now);
+         
+        if ($this->nbCoups() >= 3 && $diff->h + 24*$diff->d < 24) {
+            return self::PAS_AUJOURDHUI;
+        }
+         
+        if ($diff->h + 24*$diff->d < 24){
+            $this->setNbCoups($this->nbCoups() + 1);
+        } else {
+            $this->setNbCoups(1);
+        }
+         
+         
+        $this->setDateDernierCoup($now->format('Y-m-d'));
+         
+        return $perso->recevoirDegats($this->niveau() * 5);
+    }
+ 
+    public function recevoirDegats($force)
+    {
+        $this->setDegats($this->degats() + $force);      
+        if ($this->degats() >= 100){
+            return self::PERSONNAGE_TUE;
+        }
+        return self::PERSONNAGE_FRAPPE;
+    }
+     
+    public function gagnerExperience(){
+        $this->setExperience($this->experience() + $this->niveau() * 5);
+         
+        if ($this->experience() >= 100){
+            $this->setNiveau($this->niveau() + 1);
+            $this->setExperience(0);
+        }
+    }
+     
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+            if (method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }
+    }
+     
+    public function id()
+    {
+        return $this->_id;
+    }
+     
+    public function nom()
+    {
+        return $this->_nom;
+    }
+     
+    public function degats()
+    {
+        return $this->_degats;
+    }
+     
+    public function experience(){
+        return $this->_experience;
+    }
+     
+    public function niveau()
+    {
+        return $this->_niveau;
+    }
+     
+    public function nbCoups()
+    {
+        return $this->_nbCoups;
+    }
+     
+    public function dateDernierCoup()
+    {
+        return $this->_dateDernierCoup;
+    }
+     
+    public function dateDerniereConnexion()
+    {
+        return $this->_dateDerniereConnexion;
+    }
+     
+    public function setId($id)
+    {
+        $id = (int) $id;
+        if ($id >= 0) {
+            $this->_id = $id;
+        }
+    }
+     
+    public function setNom($nom)
+    {
+        if (is_string($nom)) {
+            $this->_nom = $nom;
+        }
+    }
+     
+    public function setDegats($degats)
+    {
+        $degats = (int) $degats;
+        //if ($degats >= 0 && $degats <= 100) {
+            $this->_degats = $degats;
+        //}
+    }
+     
+    public function setExperience($experience)
+    {
+        $experience = (int) $experience;
+        //if ($experience >= 0 && $experience <= 100) {
+            $this->_experience = $experience;
+        //}
+    }
+     
+    public function setNiveau($niveau)
+    {
+        $niveau = (int) $niveau;
+        if ($niveau >= 0 && $niveau <= 100) {
+            $this->_niveau = $niveau;
+        }
+    }
+     
+    public function setNbCoups($nbCoups)
+    {
+        $nbCoups = (int) $nbCoups;
+        if ($nbCoups >= 0 && $nbCoups <= 100) {
+            $this->_nbCoups = $nbCoups;
+        }
+    }
+     
+    public function setDateDernierCoup($dateDernierCoup)
+    {
+        $dateDernierCoup = DateTime::createFromFormat("Y-m-d", $dateDernierCoup);
+        $this->_dateDernierCoup = $dateDernierCoup;
+    }
+     
+    public function setDateDerniereConnexion($dateDerniereConnexion)
+    {
+        $dateDerniereConnexion = DateTime::createFromFormat("Y-m-d", $dateDerniereConnexion);
+        $this->_dateDerniereConnexion = $dateDerniereConnexion;
+    }
+     
+    public function nomValide()
+    {
+        return !(empty($this->_nom));
+    }
 }
